c7092644838dfe38865bba1ab585bdca97ca7056
Sa <pd.dp.83@mail.ru>
2023-02-24 22:21:33 +0300

    Созданы вспомогательные методы

--- test_add_grouppp.py
+++ test_add_grouppp.py
@@ -16,17 +16,22 @@ class TestAddGroup(unittest.TestCase):
 
     def test_add_group(self):
         wd = self.wd
-        # открытие главной страницы
-        wd.get("http://localhost/addressbook/")
-        # login
-        wd.find_element_by_name("user").clear()
-        wd.find_element_by_name("user").send_keys("admin")
-        wd.find_element_by_name("pass").click()
-        wd.find_element_by_name("pass").clear()
-        wd.find_element_by_name("pass").send_keys("secret")
-        wd.find_element_by_xpath("//input[@value='Login']").click()
-        # открытие страницы группы
-        wd.find_element_by_link_text("groups").click()
+        self.open_home_page(wd)
+        self.login(wd)
+        self.open_groups_page(wd)
+        self.create_group(wd)
+        self.return_to_groups_page(wd)
+        self.logout(wd)
+
+    def logout(self, wd):
+        # выход
+        wd.find_element_by_link_text("Logout").click()
+
+    def return_to_groups_page(self, wd):
+        # возврат на страницу со списком групп
+        wd.find_element_by_link_text("group page").click()
+
+    def create_group(self, wd):
         # создание новой группы
         wd.find_element_by_name("new").click()
         # заполнение формы
@@ -39,13 +44,26 @@ class TestAddGroup(unittest.TestCase):
         wd.find_element_by_name("group_footer").click()
         wd.find_element_by_name("group_footer").clear()
         wd.find_element_by_name("group_footer").send_keys("test_group_footer")
-        # нажатие на enter information
+        # подтверждение
         wd.find_element_by_name("submit").click()
-        # возвращение на страницу со списком групп
-        wd.find_element_by_link_text("group page").click()
-        # выход
-        wd.find_element_by_link_text("Logout").click()
-    
+
+    def open_groups_page(self, wd):
+        # открытие страницы группы
+        wd.find_element_by_link_text("groups").click()
+
+    def login(self, wd):
+        # login
+        wd.find_element_by_name("user").clear()
+        wd.find_element_by_name("user").send_keys("admin")
+        wd.find_element_by_name("pass").click()
+        wd.find_element_by_name("pass").clear()
+        wd.find_element_by_name("pass").send_keys("secret")
+        wd.find_element_by_xpath("//input[@value='Login']").click()
+
+    def open_home_page(self, wd):
+        # открытие главной страницы
+        wd.get("http://localhost/addressbook/")
+
     def is_element_present(self, how, what):
         try: self.wd.find_element(by=how, value=what)
         except NoSuchElementException as e: return False
