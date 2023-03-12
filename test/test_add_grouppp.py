a45db9e1d1422d555655c3ccdcfacfacf493113b
Sa <pd.dp.83@mail.ru>
2023-03-12 23:33:33 +0300

    Сделана общая для всех тестов фикстура в файле conftest.py

--- test/test_add_grouppp.py
+++ test/test_add_grouppp.py
@@ -1,19 +1,5 @@
 # -*- coding: utf-8 -*-
-import pytest
 from model.group import Group
-from fixture.application import Application
-
-# инициализация фикстуры
-
-# такая метка (@pytest.fixture()) превращает функцию (app) в инициализатор фикстуры
-# функция app создает фикстуру, т.е. объект типа Application и возвращать эту фикстуру
-# для разрушения фикстуры в функцию, которая инициализирует фикстуру (app) может передаваться специальный параметр (request)
-@pytest.fixture
-def app(request):
-    fixture = Application()  # объект типа Application
-    # указание на то, как эта фикстура должна быть разрушена
-    request.addfinalizer(fixture.destroy)  # у параметра request есть метод addfinalizer, а передать надо fixture.destroy
-    return fixture  # возвращает фикстуру
 
 
 # тестовый метод
