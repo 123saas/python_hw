66a1dee9d0193a6f2a6697e20fdf27e509acfa69
Sa <pd.dp.83@mail.ru>
2023-04-17 17:17:17 +0300

    Доработка вспомогательного класса, реализующего операции login и logout (SessionHelper). Добавлена проверка текущего состояния сессии и выполняются действия с учётом текущего состояния.

--- conftest.py
+++ conftest.py
@@ -17,14 +17,12 @@ def app(request):
     if fixture is None:
         # нужно фикстуру проинициализировать
         fixture = Application()  # объект типа Application
-        # после инициализации выполняем login
-        fixture.session.login(username="admin", password="secret")
         # альтернативная ситуация. предположим, что фикстура уже создана и надо проверить не испортилась ли она, поэтому добавляем вторую проверку
     else:
         if not fixture.is_valid():
             fixture = Application()  # объект типа Application
-            # после инициализации выполняем login
-            fixture.session.login(username="admin", password="secret")
+    fixture.session.ensure_login(username="admin", password="secret") # строчку нужно выполнять не только после того, как браузер запущен, а выносят сюда, чтобы она выполнялась при каждом обращении к функции (def app(request)) инициализирующей фикстуру ->
+    # -> точно так же как и с logout нам не надо выполнять login, нам надо выполнять его интеллектуально, поэтому сделаем вспомогательный метод ensure_login, который будет выполнять проверку "Нужно выполнять login или не нужно?". Eсли мы уже залогинились как правильные пользователи, то делать ничего не надо
     return fixture
 
 # для финализации делаем отдельную фикстуру
@@ -32,7 +30,7 @@ def app(request):
 def stop(request):
     # определяем маленькую локальную функцию
     def fin():
-        fixture.session.logout()
+        fixture.session.ensure_logout()  # убедиться, что мы вышли из системы
         fixture.destroy()
     # указание на то, как эта фикстура должна быть разрушена
     request.addfinalizer(fin)  # в качестве финализатора теперь будет добавлена самодельная функция fin, которая будет выполнять сразу 2 действия и logout и destroy
