e4090e0975a1fabef21c0f02f4e90c6df903e3ca
Sa <pd.dp.83@mail.ru>
2023-07-03 00:27:06 +0300

    5. Строки. Задание №16: Реализовать опции командной строки, позволяющие указывать тип браузера и адрес тестируемого приложения

--- fixture/application.py
+++ fixture/application.py
@@ -6,12 +6,20 @@ from fixture.contact import ContactHelper
 
 class Application:
 # конструктор, который инициализирует ссылку на драйвер, потом инициализирует помощников (SessionHelper,GroupHelper, ContactHelper)
-    def __init__(self):
-        self.wd = webdriver.Firefox(executable_path='C:\Windows\SysWOW64\geckodriver.exe')  # запуск драйвера
+    def __init__(self, browser, base_url):
+        if browser == "firefox":
+            self.wd = webdriver.Firefox(executable_path='C:\Windows\SysWOW64\geckodriver.exe')  # запуск драйвера
+        elif browser == "chrome":
+            self.wd = webdriver.Chrome(executable_path='C:\Windows\System32\chromedriver.exe')  # запуск драйвера
+        elif browser == "ie":
+            self.wd = webdriver.Ie(executable_path='C:\Windows\System32\IEDriverServer.exe')  # запуск драйвера
+        else:
+            raise ValueError("Unrecognized browser %s" % browser)
         self.wd.implicitly_wait(1)
         self.session = SessionHelper(self) # вот так помощник (SessionHelper) получается ссылку на объект класса Application. Это даст возможность в одном помощнике обратиться к какому-то другому помощнику через объект класса Application
         self.group = GroupHelper(self)
         self.contact = ContactHelper(self)
+        self.base_url = base_url
 
     def is_valid(self):
         # try except - это блок с перехватом исключений. если при выполнении этого блока возникают какие-то проблемы, генерируется исключение, то мы можем его перехватить и в блоке except написать какой-то обработчик, то есть как-то отреагировать на эту проблемную ситуацию. здесь мы это и делаем
@@ -29,7 +37,7 @@ class Application:
     def open_home_page(self):
         # открытие главной страницы
         wd = self.wd
-        wd.get("http://localhost/addressbook/")
+        wd.get(self.base_url)
 
 # метод, который разрушает фикстуру (destroy), в частности, он останавливает браузер
     def destroy(self):
