77b1bd2c840f4e1f82dde8eeb3335aacea2bf4ca
Sa <pd.dp.83@mail.ru>
2023-07-23 23:18:51 +0300

    Попытка 3. Задание №19: Реализовать загрузку тестовых данных из файлов

--- generator/contact.py
+++ generator/contact.py
@@ -7,7 +7,7 @@ import json
 import jsonpickle
 
 def random_string(prefix, maxlen):
-    symbols = string.ascii_letters + string.digits + string.punctuation + " "*10
+    symbols = string.ascii_letters + string.digits + " "*10
     return prefix + "".join([random.choice(symbols) for i in range(random.randrange(maxlen))])
 
 testdata = [Contact("", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "-", "-", " ", "-",
@@ -16,8 +16,8 @@ testdata = [Contact("", "", "", "", "", "", "", "", "", "", "", "", "", "", "", 
             lastname=random_string("lastname", 10), nickname=random_string("nickname", 10),
             title=random_string("title", 10), company=random_string("company", 10), address=random_string("address", 10),
             home_phone=random_string("home_phone", 10), mobile_phone=random_string("mobile_phone", 10), work_phone=random_string("work_phone", 10),
-            fax=random_string("fax", 10), email=random_string("email", 0), email2=random_string("email2", 0),
-            email3=random_string("email3", 0), homepage=random_string("homepage", 10), bday='16',
+            fax=random_string("fax", 10), email=random_string("email", 10), email2=random_string("email2", 10),
+            email3=random_string("email3", 10), homepage=random_string("homepage", 10), bday='16',
             bmonth='April', byear=random_string("b", 4), aday='18',
             amonth='May', ayear=random_string("a", 4), address2=random_string("address2", 10),
             home_phone2=random_string("home_phone2", 10), notes=random_string("notes", 10))
